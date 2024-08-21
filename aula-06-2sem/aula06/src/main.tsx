0bf1089c5b3d671a4577805bcf55dc22b809c8d4
LuizHNR <luizhneri12@gmail.com>
2024-08-21 09:12:42 -0300

    começo rotas

--- aula-06-2sem/aula06/src/main.tsx
+++ aula-06-2sem/aula06/src/main.tsx
@@ -1,14 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import Home from './routes/Home/index.tsx'
-import EditarProdutos from './routes/EditarProdutos/index.tsx'
-import Menu from './routes/Menu/index.tsx'
-import Produto from './routes/Produtos/index.tsx'
-import Error from './routes/Error/index.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home/index.tsx';
+import Menu from './routes/Menu/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Produto from './routes/Produtos/index.tsx';
+import Error from './routes/Error/index.tsx';
+
+const routes = createBrowserRouter([{
+  path:"/", element: <App/>, 
+  errorElement: <Error/>,
+  children: [
+    {path: "/", element: <Home/>},
+    {path: "/produtos", element: <Produto/>},
+    {path: "/produtos/editar/:id", element: <EditarProdutos/>},
+  ]
+
+}]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+
+    <RouterProvider routes = {routes}/>
+
   </StrictMode>,
 )
