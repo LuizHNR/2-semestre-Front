40e5a1d8aad95d270e644fe573ae5b001319cf8c
LuizHNR <luizhneri12@gmail.com>
2024-08-27 08:39:14 -0300

    Criando o array de rotas

--- aula-07-2sem/aula07/src/main.tsx
+++ aula-07-2sem/aula07/src/main.tsx
@@ -1,6 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// --------------------Importes utilizadas nas Rotas-------------------------
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import Home from './routes/Home/index.tsx';
+import Error from './routes/Error/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+
+//---------------------Construindo o array de objetos de rotas----------------
+const router = createBrowserRouter([
+  {path:"/",element:<App/>,errorElement:<Error/>,children:[
+    {path:"/",element:<Home/>},
+    {path:"/produtos",element:<Produtos/>},
+    {path:"/editar/produtos/:id",element:<EditarProdutos/>},
+  ]}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
