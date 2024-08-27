da10cb0998b529ecead7faf64ceb8c2b14782fa8
LuizHNR <luizhneri12@gmail.com>
2024-08-27 09:16:37 -0300

    uso do useState

--- aula-07-2sem/aula07/src/routes/EditarProdutos/index.tsx
+++ aula-07-2sem/aula07/src/routes/EditarProdutos/index.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function EditarProdutos(){
+
+    //Realizando um destructuring para receber o paramtredo do Hook useParams()
+
+    // let nr:number=0;
+
+    // const {id} = useParams();
+    // if (id){
+    //      nr = parseInt(id);
+    // }
+    
+    // let msg:string = "";
+    // if(typeof nr != "number"){
+    //     msg = "A informação passada está incorreta";
+    // }
+
+    const[count, setCount] = useState(0);
+
+    const incrementaNumero = () => {
+        
+    }
+
 
     return(
         <div>
-            <h1>Componente Editar Produtos</h1>
+            <h1>Componente Editar Produtos - {count} </h1>
+            <button onClick={()=> setCount(count+1)}>Contador</button>
         </div>
     );
 }
